b6bae9cd4d857e173f074d204d2f203b9ae1439b
weihaoxuan1 <277933716@qq.com>
2015-09-21 12:23:41 +0800

    done Deck.cs

--- unity/Assets/Scripts/Deck.cs
+++ unity/Assets/Scripts/Deck.cs
@@ -10,16 +10,43 @@ public class Deck : MonoBehaviour {
 	{
 		Instance = this;
 	}
-
-    public GameObject deck;
+    /// <summary>
+    /// 标准牌堆
+    /// </summary>
+    public GameObject[] standardCardPile;
+    /// <summary>
+    /// 当前牌堆的list
+    /// </summary>
     public BetterList<GameObject> cardPile;
+    /// <summary>
+    /// 当前牌堆的数组
+    /// </summary>
     public GameObject[] arrayCardPile;
+    /// <summary>
+    /// 弃牌堆的list
+    /// </summary>
     public BetterList<GameObject> discardPile;
-    public int pTop = 0;
-    public int restCardsNumber = 0;
-    Card_Sha sha;
-    Card_Shan shan;
-    Card_Tao tao;
+    /// <summary>
+    /// 弃牌堆的数组
+    /// </summary>
+    public GameObject[] arrayDiscardPile;
+    /// <summary>
+    /// 剩余牌的张数
+    /// </summary>
+    int _restCardsNumber;
+    public int restCardsNumber
+    {
+        get { return _restCardsNumber; }
+        set
+        {
+            _restCardsNumber = value;
+            if (_restCardsNumber <= 0)
+                WashDeck();
+        }
+    }
+    //Card_Sha sha;
+    //Card_Shan shan;
+    //Card_Tao tao;
 	// Use this for initialization
     public void Reset()
     {
@@ -33,79 +60,203 @@ public class Deck : MonoBehaviour {
         else Debug.Log("Deck is not null");
 	    cardPile = new BetterList<GameObject>();
         discardPile = new BetterList<GameObject>();
-        sha = new Card_Sha();
-        shan = new Card_Shan();
-        tao = new Card_Tao();
-        arrayCardPile = new GameObject[52];
-        //Debug.Log("aaaa"+ shan.name);
+        //sha = new Card_Sha();
+        //shan = new Card_Shan();
+        //tao = new Card_Tao();
+        standardCardPile = new GameObject[52];
+        //读取Resources文件夹下的所有卡牌预制
+        standardCardPile = Resources.LoadAll<GameObject>("AllCards");
         InitDeck();
-        /*for (int i = 0; i < 50; i++)
-        {
-            Debug.Log(cardPile[i].name);
-        }*/
-        //Debug.Log(cardPile[1] == null ? "null" : "not null");
-        //Debug.Log(cardPile[1].name);
+        //for (int i = 0; i < 50; i++)
+        //{
+        //    Debug.Log(arrayCardPile[i].name);
+        //}
+        InvokeRepeating("SetArrayCardPile", 1f, 1f);
 	}
-
-    //初始化牌堆，包括第一次洗牌
+    /*
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 100, 100), "mopai"))
+        {
+            DrawCard();
+        }
+        if (GUI.Button(new Rect(100, 0, 100, 100), "panding"))
+        {
+            Judge();
+        }
+        if (GUI.Button(new Rect(200, 0, 100, 100), "mopai2"))
+        {
+            DrawCard(2);
+        }
+        if (GUI.Button(new Rect(300, 0, 100, 100), "qipai3"))
+        {
+            DiscardFormDeckTop(3);
+        }
+        if (GUI.Button(new Rect(400, 0, 100, 100), "qipai"))
+        {
+            DiscardFormDeckTop();
+        }
+        if (GUI.Button(new Rect(500, 0, 100, 100), "zhanshi"))
+        {
+            print(ShowCardFromDeckTop().name);
+        }
+        if (GUI.Button(new Rect(600, 0, 100, 100), "zhanshi3"))
+        {
+            GameObject[] t= ShowCardFromDeckTop(3);
+            for(int i=0;i<t.Length;i++)
+                print(t[i].name);
+        }
+    }
+    */
+    /// <summary>
+    /// 初始化牌堆，洗牌,一局游戏结束后再调用
+    /// </summary>
     void InitDeck()
     {
-        int totalCardNum = gameObject.transform.childCount;
-        //获取deck对象下的全部卡牌并存入cardpile
-        for(int i = 0;i<totalCardNum;i++)
+        //int totalCardNum = gameObject.transform.childCount;
+        ////获取deck对象下的全部卡牌并存入cardpile
+        //for(int i = 0;i<totalCardNum;i++)
+        //{
+        //    GameObject temp = gameObject.transform.GetChild(i).gameObject;
+        //    cardPile.Add(temp);
+        //}
+        for (int i = 0; i < standardCardPile.Length; i++)
         {
-            GameObject temp = gameObject.transform.GetChild(i).gameObject;
-            cardPile.Add(temp);
+            cardPile.Add(standardCardPile[i]);
         }
         BetterList<GameObject> tempList = new BetterList<GameObject>();
         //第一次洗牌
-        for(int i=0;i<totalCardNum;i++)
+        for (int i = 0; i < standardCardPile.Length; i++)
         {
             int index = Random.Range(0,cardPile.size);
             tempList.Add(cardPile[index]);
             cardPile.RemoveAt(index);
         }
+        restCardsNumber = tempList.size;
         cardPile = tempList;
-        arrayCardPile = cardPile.ToArray();
-        print("finish init");
+        //arrayCardPile = cardPile.ToArray();
+        print("finish init Deck");
     }
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
 
+    void SetArrayCardPile()
+    {
+        print("SetArrayCardPile");
+        arrayCardPile = cardPile.ToArray();
+    }
+
+    /// <summary>
+    /// 从牌堆顶摸牌
+    /// </summary>
+    /// <returns></returns>
     public GameObject DrawCard()
     {
-        //pTop--;
+        //防止还没有pop掉牌堆最后一张牌就洗牌,因此先pop再rest--
+        GameObject temp = cardPile.Pop();
         restCardsNumber--;
-        Debug.Log("draw a " + cardPile[pTop].name);
-        Debug.Log("Deck rest " + restCardsNumber);
-        return cardPile[pTop--];
+        Debug.Log("draw a " + temp.name);
+        return temp;
+        //MainProcess.Instance.NextStage();
+    }
+    public GameObject[] DrawCard(int num)
+    {
+        GameObject[] temps = new GameObject[num];
+        for (int i = 0; i < num; i++)
+        {
+            //防止还没有pop掉牌堆最后一张牌就洗牌,因此先pop再rest--
+            GameObject temp = cardPile.Pop();
+            restCardsNumber--;
+            Debug.Log("draw a " + temp.name);
+            temps[i] = temp;
+        }
+        return temps;
         //MainProcess.Instance.NextStage();
     }
 
+    /// <summary>
+    /// 从牌堆顶判定一张牌
+    /// </summary>
+    /// <returns></returns>
     public GameObject Judge()
     {
-        Debug.Log("judged a ");
-        return cardPile[pTop--];
+        Debug.Log("judged a " + cardPile[restCardsNumber - 1].name);
+        //防止还没有pop掉牌堆最后一张牌就洗牌,因此先pop再rest--
+        GameObject temp = cardPile.Pop();
+        restCardsNumber--;
+        //Warning判定后不一定马上进弃牌堆,以后有相关逻辑时要改
+        return DiscardCard(temp);
     }
 
-    public void WashDeck(){
+    /// <summary>
+    /// 从牌堆顶弃牌
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public GameObject DiscardFormDeckTop()
+    {
+        //防止还没有pop掉牌堆最后一张牌就洗牌,因此先pop再rest--
+        GameObject temp = cardPile.Pop();
+        restCardsNumber--;
+        return DiscardCard(temp);
+    }
+    public GameObject[] DiscardFormDeckTop(int num)
+    {
+        GameObject[] temps = new GameObject[num];
+        for (int i = 0; i < num; i++)
+        {
+            //防止还没有pop掉牌堆最后一张牌就洗牌,因此先pop再rest--
+            GameObject temp = cardPile.Pop();
+            restCardsNumber--;
+            temps[i] = DiscardCard(temp);
+        }
+        return temps;
+    }
+
+    /// <summary>
+    /// 从牌堆顶展示一张牌
+    /// </summary>
+    /// <returns></returns>
+    public GameObject ShowCardFromDeckTop()
+    {
+        return cardPile[cardPile.size - 1];
+    }
+    public GameObject[] ShowCardFromDeckTop(int num)
+    {
+        GameObject[] temps = new GameObject[num];
+        for (int i = 0; i < num; i++)
+        {
+            temps[i] = cardPile[cardPile.size - 1 - i];
+        }
+        return temps;
+    }
+
+    /// <summary>
+    /// 洗牌,把弃牌堆的牌随机放回牌堆,根据规则请在牌堆没牌后才调用
+    /// </summary>
+    public void WashDeck()
+    {
+        //Warning现在的洗牌算法中最后一张牌不会洗入新的牌堆,以后看看要不要改
         int rest = discardPile.size;
         for (int i = 0; i < rest; i++)
         {
-            int index = Random.Range(0, rest-i);
+            int index = Random.Range(0, discardPile.size);
             cardPile.Add(discardPile[index]);
             discardPile.RemoveAt(index);
         }
+        restCardsNumber = cardPile.size;
         print("finish wash deck");
     }
 
-    public void DiscardCard(GameObject c)
+    public GameObject DiscardCard(GameObject c)
     {
         discardPile.Add(c);
-		Player_Man.Instance.CheckHoldCards ();
-		Player_Enemy.Instance.CheckHoldCards ();
+        arrayDiscardPile = discardPile.ToArray();
+        //Player_Man.Instance.CheckHoldCards ();
+        //Player_Enemy.Instance.CheckHoldCards ();
+        return c;
     }
 
     /*void OnStageDel(MainProcess.StageEvent stageEvent)
---
+++ unity/Assets/Scripts/Tools/CreateAssetbundle.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using System.IO;
+
+public class CreateAssetbundle : Editor {
+
+	//打包单个
+    [MenuItem("Custom Editor/Create AssetBundles Main(Android)")]
+    static void CreateAssetBundlesMain_Android()
+    {
+        string path = AssetbundlePathAndroid;
+        //获取在project视图中选择的所有游戏对象
+        Object[] SelectedAsset = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+
+        //遍历所有游戏对象
+        foreach (Object obj in SelectedAsset)
+        {
+            Caching.CleanCache();
+
+            //本地测试：建议最好将Assetbundle房子StreamingAssets文件夹下，
+            //如果没有就创建一个，因为移动平台下只能读取这个路径
+            //StreamingAssets是只读路径，不能写入
+            //服务器下载:就不需要放这里了
+            //string targetPath = Application.dataPath + "/StreamingAssets/" + obj.name + ".assetbundle";
+            string targetPath = path;
+
+            /*
+                默认情况下打的包只能在电脑上用，如果要在手机上用就要添加一个参数。
+                Android上：
+                BuildPipeline.BuildAssetBundle (obj, null, targetPath, BuildAssetBundleOptions.CollectDependencies,BuildTarget.Android)
+                IOS上:
+                BuildPipeline.BuildAssetBundle (obj, null, targetPath, BuildAssetBundleOptions.CollectDependencies,BuildTarget.iPhone)
+                另外，电脑上和手机上打出来的Assetbundle不能混用，不同平台只能用自己的。
+            */
+            if (BuildPipeline.BuildAssetBundle(obj, null, targetPath, BuildAssetBundleOptions.CollectDependencies, BuildTarget.Android))
+            {
+                Debug.Log(obj.name + "资源打包成功");
+            }
+            else {
+                Debug.Log(obj.name + "资源打包失败");
+            }
+        }
+        //刷新编辑器
+        AssetDatabase.Refresh();
+    }
+    //打包单个
+    //[MenuItem("Custom Editor/Create AssetBundles Main(IOS)")]
+    //static void CreateAssetBundlesMain_IOS()
+    //{
+    //    string path = AssetbundlePathIOS;
+    //    //获取在project视图中选择的所有游戏对象
+    //    Object[] SelectedAsset = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+
+    //    //遍历所有游戏对象
+    //    foreach (Object obj in SelectedAsset)
+    //    {
+    //        Caching.CleanCache();
+
+    //        //本地测试：建议最好将Assetbundle房子StreamingAssets文件夹下，
+    //        //如果没有就创建一个，因为移动平台下只能读取这个路径
+    //        //StreamingAssets是只读路径，不能写入
+    //        //服务器下载:就不需要放这里了
+    //        //string targetPath = Application.dataPath + "/StreamingAssets/" + obj.name + ".assetbundle";
+    //        string targetPath = path;
+
+    //        /*
+    //            默认情况下打的包只能在电脑上用，如果要在手机上用就要添加一个参数。
+    //            Android上：
+    //            BuildPipeline.BuildAssetBundle (obj, null, targetPath, BuildAssetBundleOptions.CollectDependencies,BuildTarget.Android)
+    //            IOS上:
+    //            BuildPipeline.BuildAssetBundle (obj, null, targetPath, BuildAssetBundleOptions.CollectDependencies,BuildTarget.iPhone)
+    //            另外，电脑上和手机上打出来的Assetbundle不能混用，不同平台只能用自己的。
+    //        */
+    //        if (BuildPipeline.BuildAssetBundle(obj, null, targetPath, BuildAssetBundleOptions.CollectDependencies, BuildTarget.iOS))
+    //        {
+    //            Debug.Log(obj.name + "资源打包成功");
+    //        }
+    //        else
+    //        {
+    //            Debug.Log(obj.name + "资源打包失败");
+    //        }
+    //    }
+    //    //刷新编辑器
+    //    AssetDatabase.Refresh();
+    //}
+
+    //打包多个
+    [MenuItem("Custom Editor/Create AssetBundles All (Andorid)")]
+    static void CreateAssetBundlesAll_Andorid()
+    {
+        Caching.CleanCache();
+
+        string path = Application.dataPath + "/AssetBundleAndroid/sound.assetbundle";
+        Object[] SelectAsset = Selection.GetFiltered(typeof(object), SelectionMode.DeepAssets);
+        foreach (Object obj in SelectAsset)
+        {
+            Debug.Log("Create AssetBundles name :" + obj.name);
+        }
+        if (BuildPipeline.BuildAssetBundle(null, SelectAsset, path, BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.Android))
+        {
+            AssetDatabase.Refresh();
+        }
+    }
+    //打包多个
+    //[MenuItem("Custom Editor/Create AssetBundles All (IOS)")]
+    //static void CreateAssetBundlesAll_IOS()
+    //{
+    //    Caching.CleanCache();
+
+    //    string path = Application.dataPath + "/AssetBundleIOS/sound.assetbundle";
+    //    Object[] SelectAsset = Selection.GetFiltered(typeof(object), SelectionMode.DeepAssets);
+    //    foreach (Object obj in SelectAsset)
+    //    {
+    //        Debug.Log("Create AssetBundles name :" + obj.name);
+    //    }
+    //    if (BuildPipeline.BuildAssetBundle(null, SelectAsset, path, BuildAssetBundleOptions.CollectDependencies, BuildTarget.iOS))
+    //    {
+    //        AssetDatabase.Refresh();
+    //    }
+    //}
+
+    //打包场景
+    [MenuItem("Custom Editor/Create Scene")]
+    static void CreateSceneAll()
+    { 
+        //清空一下缓存
+        Caching.CleanCache();
+
+        string path = Application.dataPath + "/ZJHScene.unity3d";
+        string[] levels = { "Assets/scene/ZJH.unity" };
+        //打包场景
+        BuildPipeline.BuildPlayer(levels, path, BuildTarget.WebPlayer, BuildOptions.BuildAdditionalStreamedScenes);
+        AssetDatabase.Refresh();
+    }
+
+
+    public static string AssetbundlePath1
+    {
+        get { return "Assets" + Path.DirectorySeparatorChar; }
+    }
+
+    public static string AssetbundlePathAndroid
+    {
+        get
+        {
+            // 打开保存面板，获得用户选择的路径  
+            string path = EditorUtility.SaveFilePanel("Save Resource", AssetbundlePath1 + "AssetBundleAndroid" + Path.DirectorySeparatorChar, "New Resource", "assetbundle");
+            return path;
+        }
+    }
+    public static string AssetbundlePathIOS
+    {
+        get
+        {
+            // 打开保存面板，获得用户选择的路径  
+            string path = EditorUtility.SaveFilePanel("Save Resource", AssetbundlePath1 + "AssetBundleIOS" + Path.DirectorySeparatorChar, "New Resource", "assetbundle");
+            return path;
+        }
+    }
+
+}
